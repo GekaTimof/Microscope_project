3fed85c79163d2a3461b0d3ae74969bd2513e137
Evgeniy Timoffev <geka.timof.max@gmail.com>
2025-02-18 19:51:36 +0000

    Start checking connection to real spectrometer

--- Spectrometer/Visualization/connection.py
+++ Spectrometer/Visualization/connection.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import pexpect
 
 # Полный путь к исполняемому файлу
@@ -9,17 +10,30 @@ get_data_pass = os.path.join(working_directory, "OptoskyDemo")
 # Запускаем процесс
 child = pexpect.spawn(get_data_pass, cwd=working_directory, encoding="utf-8", timeout=10)
 
-# Читаем вывод до появления "Enter :"
-child.expect("Enter :")
-print(child.before)  # Вывод меню
+# try to start script (this script tp get data from spectrometer)
+try:
+    # Читаем вывод до появления "Enter :"
+    child.expect("Enter :", timeout=5)
+    print(child.before)  # Вывод меню
+except:
+    print("Нет ответа од спектрометра, проверьте подключение")
+    sys.exit(1)
 
-# Отправляем команду (например, "2" для API Get vendor)
-child.sendline("2")
+# try to connect to spectrometer
+try:
+    # Отправляем команду (например, "0" для подключения к спектрометру)
+    child.sendline("0")
+    child.expect("success!", timeout=5)
+    print("Подключене прошло успешно")
+except:
+    print("Не удолось подключиться к спектрометру, проверьте ваши права (sudo)")
+    sys.exit(1)
 
 # Читаем ответ
 child.expect("Enter :")
 print(child.before)  # Вывод после команды
 
 # Завершаем программу (отправляем команду выхода "100")
+print("Завершаем сеанс")
 child.sendline("100")
 child.close()

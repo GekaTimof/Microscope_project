5291edb5fa2b95d9fec08fbd0cb238d52ec4e07b
Evgeniy Timoffev <geka.timof.max@gmail.com>
2025-07-03 19:04:53 +0000

    Add guide to README.md (how to create application icon)

--- Spectrometer/run_with_sudo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-PROJECT_ROOT=$(realpath "$SCRIPT_DIR")
-VISUALIZATION_DIR="$PROJECT_ROOT/Visualization"
-
-sudo env "PATH=$PATH" poetry run python3 "$VISUALIZATION_DIR/main.py" "$@"
-
